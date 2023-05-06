8262b6a6a762a845e32d3cf29bad78566679ee3c
jwo0o0 <jwo0o0358@gmail.com>
2023-05-06 10:02:26 +0900

    feat: proxy 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -7,5 +6,12 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });
