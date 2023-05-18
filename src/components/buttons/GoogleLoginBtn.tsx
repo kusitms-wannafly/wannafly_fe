e20bf9813d6e8f17de98879780e5342788876657
jwo0o0 Kim <jwo0o0358@gmail.com>
2023-05-18 16:01:40 +0900

    feat: 네이버, 카카오 소셜 로그인 추가, 로그인 에러 처리 (#21)
    * feat: 네이버, 카카오 로그인 버튼 추가
    
    * fix: 소셜로그인 에러 처리
    
    * chore: 필요 없는 코드 삭제

--- src/components/buttons/GoogleLoginBtn.tsx
+++ src/components/buttons/GoogleLoginBtn.tsx
@@ -1,17 +1,12 @@
 import styled from 'styled-components';
 import google_login_btn_url from '@assets/images/btn_google_signin_light_normal_web@2x.png';
-import { handleGoogleLogin } from '@features/oauth/handleGoogleLogin';
 
 export const GoogleLoginBtn = () => {
   return (
     <BtnContainer
       href={`${import.meta.env.VITE_API_URL}/oauth2/authorization/google`}
     >
-      <LoginBtnImg
-        src={google_login_btn_url}
-        alt="google social login"
-        onClick={handleGoogleLogin}
-      />
+      <LoginBtnImg src={google_login_btn_url} alt="google social login" />
     </BtnContainer>
   );
 };
