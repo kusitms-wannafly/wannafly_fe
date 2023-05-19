8fe760fdaef68da5940abf700a98d8bd30c994c4
jwo0o0 <jwo0o0358@gmail.com>
2023-05-19 21:52:58 +0900

    fix: accessToken 재발급 로직 버그로 주석처리

--- src/api/HttpClient.ts
+++ src/api/HttpClient.ts
@@ -11,40 +11,44 @@ export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      console.log('retry');
-      //무한루프 방지
-      originalRequest._retry = true;
-    }
-    //Refresh Token으로 새로운 AccessToken 요청
-    try {
-      const apiresult: Promise<any> = accessTokenAPI();
-      return apiresult.then((res) => {
-        const newAccessToken = res.accessToken;
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       console.log('retry');
+//       //무한루프 방지
+//       originalRequest._retry = true;
 
-        //새로운 AccessToken으로 원래 요청 재시도
-        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-        axiosInstance.defaults.headers.common[
-          'Authorization'
-        ] = `Bearer ${newAccessToken}`;
+//       //Refresh Token으로 새로운 AccessToken 요청
+//       try {
+//         const apiresult: Promise<any> = accessTokenAPI();
+//         return apiresult.then((res) => {
+//           const newAccessToken = res.accessToken;
 
-        //로컬스토리지에 새로운 accessToken 저장
-        localStorage.setItem('accessToken', `Bearer ${newAccessToken}`);
+//           //새로운 AccessToken으로 원래 요청 재시도
+//           originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+//           axiosInstance.defaults.headers.common[
+//             'Authorization'
+//           ] = `Bearer ${newAccessToken}`;
 
-        return axiosInstance(originalRequest);
-      });
-    } catch (err) {
-      //refresh Token이 만료된 경우 메인 페이지로 이동
-      window.location.replace('/');
-    }
-  }
-);
+//           //로컬스토리지에 새로운 accessToken 저장
+//           localStorage.setItem('accessToken', `Bearer ${newAccessToken}`);
+
+//           return axiosInstance(originalRequest);
+//         });
+//       } catch (err) {
+//         //refresh Token이 만료된 경우 메인 페이지로 이동
+//         window.location.replace('/');
+//         return Promise.reject(err);
+//       }
+//     } else {
+//       return Promise.reject(error);
+//     }
+//   }
+// );
 
 //http request 전 로컬스토리지에 있는 accessToken을 헤더에 포함
 axiosInstance.interceptors.request.use(
