c5629038dde33f5d94cdce31e04bee6124215f93
jwo0o0 <jwo0o0358@gmail.com>
2023-05-20 13:52:25 +0900

    fix: 지원서 마지막 수정 날짜 제대로 안나오는 버그 수정

--- src/pages/Applications/util/getModifiedDateString.ts
+++ src/pages/Applications/util/getModifiedDateString.ts
@@ -3,7 +3,7 @@ import { parseISO, getMonth, getDate } from 'date-fns';
 //ISO 형식의 string을 -월-일 수정 형태의 문자열로 반환
 export const getModifiedDateString = (ISODate: string) => {
   const parsedDate = parseISO(ISODate);
-  const month = getMonth(parsedDate);
+  const month = getMonth(parsedDate) + 1;
   const date = getDate(parsedDate);
 
   return `${month}월 ${date}일 수정`;
