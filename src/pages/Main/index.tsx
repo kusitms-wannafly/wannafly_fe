6872acaf0bd2f5cb2b7560b0346594b41149d395
jwo0o0 <jwo0o0358@gmail.com>
2023-05-20 21:58:07 +0900

    fix: 안쓰는 파일 삭제

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,28 +2,12 @@ import { PageContainer } from '@components/Layout/PageContainer';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import { LoginAlert } from '@features/oauth/LoginAlert';
-import { useLocation, useNavigate } from 'react-router-dom';
-
-// 지원서 보관함 모두 조회 API
-import { getAllFolderAPI } from '@api/folderAPIS';
+import { useLocation } from 'react-router-dom';
 
 import { NotLoginPage } from './components/NotLoginPage';
 import { LoginPage } from './components/LoginPage';
 
-import SelectMenu from '@pages/Main/components/SelectMenu';
-import GreyFolderImage from '@assets/images/grey-folder.png';
-import YellowFolderImage from '@assets/images/yellow-folder.png';
-//import Butterfly from '@assets/images/unionbutterfly.png';
-import { CreateFolderButton } from '@pages/Main/components/CreateFolderButton';
-import { NewFolder } from './components/NewFolder';
-
-interface Folder {
-  year: number;
-  count: number;
-}
-
 export const MainPage = () => {
-  const navigate = useNavigate();
   const location = useLocation();
   const loginState = location.state?.loginState;
 
@@ -37,37 +21,6 @@ export const MainPage = () => {
       setIsLogin(false);
     }
   }, []);
-
-  // 지원서 보관함 불러오기
-  //const [hasFolder, setHasFolder] = useState(false);
-  const [folders, setFolders] = useState<Folder[]>([]);
-
-  const getAllFolders = () => {
-    const apireturn = getAllFolderAPI();
-    apireturn
-      .then((res) => {
-        setFolders(res);
-      })
-      .catch(() => {
-        setFolders([]);
-      });
-  };
-
-  useEffect(() => {
-    getAllFolderAPI();
-  }, []);
-
-  const [selectedYear, setSelectedYear] = useState<number>(2023);
-
-  //보관함 페이지로 이동
-  const handleClickFolder = (year: number) => {
-    //전체
-    if (year === -1) {
-      navigate('/applications');
-    } else {
-      navigate(`/applications/${year}`);
-    }
-  };
 
   return (
     <PageContainer header>
@@ -83,34 +36,3 @@ const MainPageContainer = styled.div`
   padding-top: 75px;
   width: 100vw;
 `;
-
-// const FolderContainer = styled.div`
-//   padding-left: 348px;
-//   padding-top: 50px;
-//   margin-top: 250px;
-//   position: absolute;
-// `;
-
-// const YearChooseButton = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   margin-top: 105px;
-//   margin-left: 50px;
-//   position: absolute;
-// `;
-
-// const GreyFolder = styled.img`
-//   width: 280px;
-//   height: 220px;
-//   padding-right: 20px;
-// `;
-
-// const YellowFolder = styled.img`
-//   width: 280px;
-//   height: 220px;
-//   padding-right: 20px;
-// `;
-
-// const UnionButterfly = styled.img`
-//   padding-left: 1100px;
-// `;
